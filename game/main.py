df991ed2499f2fb7eeb06ac3585d58a2bf1720fb
Eugene-TechnoNinja <technoninja@ukr.net>
2023-04-27 11:35:12 +0300

    background added

--- game/main.py
+++ game/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pygame as pg
 from config import WINDOW, FPS, COLORS, GAME_NAME
@@ -7,8 +8,11 @@ def main():
     pg.init()
     clock = pg.time.Clock()
     screen = pg.display.set_mode((WINDOW['WIDTH'], WINDOW['HEIGHT']))
-    screen.fill(COLORS['MAIN_SPACE'])
     pg.display.set_caption(GAME_NAME)
+    screen.fill(COLORS['MAIN_SPACE'])
+    background = pg.image.load(os.path.join('assets', 'background.jpeg'))
+    background = pg.transform.scale(background, (WINDOW['WIDTH'], WINDOW['HEIGHT']))
+    screen.blit(background, (0, 0))
 
     while True:
         for event in pg.event.get():
